c52ad93588b9423cb8f6449a31cde87c1e0594fc
Julian <jjvll90@gmail.com>
2024-05-09 14:55:52 -0500

    Add getPostById and createPost methods to Post model

--- models/post.js
+++ models/post.js
@@ -3,5 +3,11 @@ let posts = [];
 module.exports = {
     getAllPost(){
         return posts;
+    },
+    getPostById(id){
+        return posts.find(post => post.id === id);
+    },
+    createPost(post) {
+        posts.push(post);
     }
 }
