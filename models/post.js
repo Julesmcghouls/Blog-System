e8ffa4ee84f73ac17d7a8bdf6d93fa6ea0fdd885
Julian <jjvll90@gmail.com>
2024-05-09 20:55:44 -0500

    Refactor post.js module

--- models/post.js
+++ models/post.js
@@ -1,22 +1,23 @@
+// posts.js
 let posts = [];
 
 module.exports = {
-    getAllPost(){
-        return posts;
-    },
-    getPostById(id){
-        return posts.find(post => post.id === id);
-    },
-    createPost(post) {
-        posts.push(post);
-    }, 
-    updatePost(id, udatedPost) {
-        const index = posts.findIndex(post => post.id === id);
-        if (index !== -1) {
-            posts[index] = updatedPost;
-        }
-    },
-    deletePost(id) {
-        posts = posts.filter(post => post.id !== id);
-    }
+getAllPosts() {
+return posts;
+},
+getPostById(id) {
+return posts.find(post => post.id === id);
+},
+createPost(post) {
+posts.push(post);
+},
+updatePost(id, updatedPost) {
+const index = posts.findIndex(post => post.id === id);
+if (index !== -1) {
+    posts[index] = updatedPost;
+}
+},
+deletePost(id) {
+posts = posts.filter(post => post.id !== id);
+}
 };
